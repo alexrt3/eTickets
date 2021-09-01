381e9b5c3e4e7f9448caa99385bf31a1864b81e0
alexrt3 <77557192+alexrt3@users.noreply.github.com>
2021-09-01 18:42:57 -0500

    Commit
    Seeded data into databse

--- eTickets/eTickets/Models/Movie.cs
+++ eTickets/eTickets/Models/Movie.cs
@@ -14,32 +14,24 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
-
-        public string Descrption { get; set; }
-
+        public string Description { get; set; }
         public double Price { get; set; }
-
         public string ImageURL { get; set; }
-
         public DateTime StartDate { get; set; }
-        
         public DateTime EndDate { get; set; }
-
         public MovieCategory MovieCategory { get; set; }
 
         //Relationships
         public List<Actor_Movie> Actors_Movies { get; set; }
 
-        //Cinemas
+        //Cinema
         public int CinemaId { get; set; }
         [ForeignKey("CinemaId")]
-
         public Cinema Cinema { get; set; }
 
         //Producer
         public int ProducerId { get; set; }
         [ForeignKey("ProducerId")]
-
         public Producer Producer { get; set; }
     }
 }
--- eTickets/eTickets/Startup.cs
+++ eTickets/eTickets/Startup.cs
@@ -2,6 +2,7 @@ using eTickets.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +26,7 @@ namespace eTickets
         public void ConfigureServices(IServiceCollection services)
         {
             //DbContext configuration
-            services.AddDbContext<AppDbContext>();
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaulConnectionString")));
 
             services.AddControllersWithViews();
         }
@@ -56,6 +57,9 @@ namespace eTickets
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //Seed database
+            AppDbInitializer.Seed(app);
         }
     }
 }
